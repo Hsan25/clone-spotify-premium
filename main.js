16979f7d4e9029dbaeb61d9859ad33444c5d5a2e
Your Name <you@example.com>
2023-06-28 20:35:50 +0700

    update

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ const boxMenu = document.querySelector("nav ul");
 const btnCloseMenu = document.querySelector(".close-menu");
 function showMenu(){
     boxMenu.classList.toggle("active");
+    document.body.classList.add("beku")
 }
 function hideMenu(e){
     e.preventDefault();
     boxMenu.classList.remove("active");
+    document.body.classList.remove("beku");
 }
 
 btnMenu.addEventListener("click", showMenu);
